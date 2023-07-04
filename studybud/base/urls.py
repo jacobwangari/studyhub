d24ab98972b4dde5d7ddb80df56bdda5e12a6db4
jacobwangari <mwasjac@gmail.com>
2023-07-04 10:03:37 +0300

    second day studybud

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('room/<str:pk>/', views.room, name = 'room'),
+    
+    path('create-room/', views.createRoom, name = 'createroom'),
+    path('update-room/<str:pk>/', views.updateRoom, name = 'updateroom'),
+    path('delete-room/<str:pk>/', views.deleteRoom, name = 'deleteroom'),
 ]
