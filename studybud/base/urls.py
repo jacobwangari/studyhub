f73b486e3c769f55ef8299670627f46d7d9b2f2b
jacobwangari <mwasjac@gmail.com>
2023-07-05 12:33:41 +0300

    Seach bar implementetd

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create-room/', views.createRoom, name = 'createroom'),
     path('update-room/<str:pk>/', views.updateRoom, name = 'updateroom'),
     path('delete-room/<str:pk>/', views.deleteRoom, name = 'deleteroom'),
+    
+    path('login/', views.loginPage, name = 'login'),
 ]
